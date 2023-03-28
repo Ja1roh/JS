828880828f0fe72061de3437088d7a98187c5459
jair <jair.ibarra.p@gmail.com>
2023-03-27 19:44:33 -0600

    Terminé el ejercicio 2 de la piramide

--- JS-04 - Arrays and Loops/Java/main.js
+++ JS-04 - Arrays and Loops/Java/main.js
@@ -1,3 +1,10 @@
+
+let niveles = document.getElementById("txt");
+let boton = document.getElementById("boton");
+let resultado = document.getElementById("resultado");
+
+
+
 let nombres = ["Sofía", "David", "Juan"];
 console.log("***************Ejercicio 1***************");
 //Parte 1
@@ -10,3 +17,16 @@ console.log(nombres);
 //parte 3
 nombres.splice(1, 0, "Renata");
 console.log(nombres);
+
+console.log("***************Ejercicio 2***************");
+triangulo(5)
+
+function triangulo(nPisos){
+    for (let cont = 0; cont<nPisos; cont++){
+        let piso = "";
+        for (b = 0; b<cont + 1; b++){
+            piso = piso + "*";
+        }
+        console.log(piso);
+    }
+}
